6b2a624f8cea251a439f0435a00097ac29601b19
Pinky Gautam <pinky@ddproperty.com>
2023-12-14 22:52:18 +0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -36,3 +36,13 @@ for fruit in apple banana "dragon fruit"
 do
 	echo $fruit
 done
+
+# for loop with numbers
+for i in {1..5} # generate numbers from 1 to 5
+do
+    if [[ $(($i % 4)) != 0 ]]
+    then 
+        continue # skip remaning lines and continue looping
+    fi
+    echo $i
+done
