403c660324b15ec59c03eb367a333e7234fee5a4
Pinky Gautam <pinky@ddproperty.com>
2023-12-17 01:04:39 +0700

    Update read.sh

--- file/read.sh
+++ file/read.sh
@@ -5,6 +5,6 @@ line=1
 # read file line by line
 cat "file/sample_file.txt" | while read current_line
 do
-	echo "$line: $current_line"
+	echo "line $line: $current_line"
     line=$(($line+ 1))
 done
