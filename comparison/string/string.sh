c5bb1d982bab99d2be0b430a1c9c2aecae4476c3
Pinky Gautam <pinky@ddproperty.com>
2023-12-16 20:32:39 +0700

    Update string.sh

--- comparison/string/string.sh
+++ comparison/string/string.sh
@@ -4,3 +4,9 @@ if [[ "bc" < "bcc" ]] # lexicographical comparison
 then
     echo "bc comes before bcc in dictionary"
 fi
+
+# check for awesome substring
+if echo "It's awesome" | grep -q "awesome" 
+then
+    echo "contains awesome"
+fi
