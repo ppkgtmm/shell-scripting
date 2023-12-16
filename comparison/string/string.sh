343fbb3ed2e2ec317f110f72f11a25fa255192f4
Pinky Gautam <pinky@ddproperty.com>
2023-12-16 20:56:23 +0700

    Update string.sh

--- comparison/string/string.sh
+++ comparison/string/string.sh
@@ -5,8 +5,16 @@ then
     echo "bc comes before bcc in dictionary"
 fi
 
-# check for awesome substring
-if echo "It's awesome" | grep -q "awesome"
+# check for substring awesome
+if echo "It's awesome" | grep -q "awesome" # -q = quiet
 then
     echo "contains awesome"
+fi
+
+ph="ph: 091-123-9878."
+re="(0[0-9]{2}\-[0-9]{3}\-[0-9]{4})"
+
+if [[ "$ph" =~ $re ]]; then
+    # extract matched part
+    echo $ph | grep -Eo $re # -E = allow regex, -o output matched parts
 fi
