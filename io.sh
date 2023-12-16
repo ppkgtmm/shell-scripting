c04a64aaefa762e3b9d89b5362068a9d96570256
Pinky Gautam <pinky@ddproperty.com>
2023-12-16 20:22:17 +0700

    Update io.sh

--- io.sh
+++ io.sh
@@ -2,4 +2,6 @@
 
 # get input from console and store in person_name variable
 read -p "Enter your name : " person_name
+# convert string to upper case
+person_name=$(echo $person_name | tr 'a-z' 'A-Z')
 echo "Hello $person_name" # output (print) value to console
