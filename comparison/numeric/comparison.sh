62128d7a7a949cd6728fa4e88c85889e03d07a23
Pinky Gautam <pinky@ddproperty.com>
2023-12-16 23:53:33 +0700

    Update comparison.sh

--- comparison/numeric/comparison.sh
+++ comparison/numeric/comparison.sh
@@ -3,11 +3,12 @@
 read -p "Enter first number : " num1
 read -p "Enter second number : " num2
 
-if [[ "$num1" -eq "$num2" ]]
+# safe comparison even for decimal numbers
+if [[ $(echo "${num1} == ${num2}" | bc) -eq 1 ]]
 then 
     echo 'numbers are equal'
 else
-    if [[ "$num1" -gt "$num2" ]]
+    if [[ $(echo "${num1} > ${num2}" | bc) -eq 1 ]]
     then
         echo 'first number > second number'
     else
