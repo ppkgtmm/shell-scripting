673e2dc5ae02dc4b974ccb3baab489daf4ad0d0a
Pinky Gautam <pinky@ddproperty.com>
2023-12-16 18:56:51 +0700

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,16 +1,5 @@
 #! /bin/sh
 
-# variable definition
-greeting="Hello"
-name="Linux"
-
-echo $greeting $name # variable reference
-
-exp_result=$((2*4)) # arithmetic expression
-echo $exp_result
-
-echo "scale=2;18.22/4" | bc # truncate decimal to 2 places
-
 # get input from console and store in person_name variable
 read -p "Enter your name" person_name
 echo "Hello $person_name"
