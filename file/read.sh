4ee12001ad1f8e71a8c5eace58301c43cd1b6e83
Pinky Gautam <pinky@ddproperty.com>
2023-12-17 14:29:11 +0700

    Update read.sh

--- file/read.sh
+++ file/read.sh
@@ -3,7 +3,7 @@
 line=1
 
 # read file line by line
-while read -r current_line
+while read -r current_line # -r = not discard (escape) backslash
 do
     echo "line $line: $current_line"
     line=$(($line+ 1))
