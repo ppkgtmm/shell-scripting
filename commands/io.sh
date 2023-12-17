b91477ab0f0ce80946442eca632b211653456dc6
Pinky Gautam <pinky@ddproperty.com>
2023-12-17 20:17:53 +0700

    Create io.sh

---
+++ commands/io.sh
@@ -0,0 +1,6 @@
+# pipe | passes output as input to another program
+echo "piping the pipe" | grep "pipe"
+
+# redirect > passes output to either a file or stream
+echo 1234 > file.txt # overwrites file.txt
+echo 1234 >> file.txt # appends to file.txt
