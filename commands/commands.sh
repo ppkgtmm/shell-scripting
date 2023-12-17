947e5b4f545fa94a71e48b7082881f641849d078
Pinky Gautam <pinky@ddproperty.com>
2023-12-17 20:17:49 +0700

    Create commands.sh

---
+++ commands/commands.sh
@@ -0,0 +1,20 @@
+# FOR EXAMPLE ONLY
+
+pwd # prints current directory
+which program # find program file in path variable
+ls dir_name # list directory content
+touch file_name # creates a new file
+mkdir dir_name # creates a new directory
+cd target_dir # change directory to target_dir
+mv source target # move file or directory from source to target
+
+cp source target # copy file from source to target
+cp -r source target # copy directory from source to target
+
+rm target # remove target file
+rm -r target # remove target directory
+
+cat file1 file2 # read and concatenate file contents
+grep pattern file # search for text in file
+man command # print command manual
+sleep seconds # wait for specified seconds
